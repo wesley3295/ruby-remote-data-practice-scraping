f1c612dfe9b654e717589f21682c81852679b70a
Wesley <wspencer3295@yahoo.com>
2020-11-28 11:07:38 -0700

    commit tree 9a2e578b0592fa13accbde6f69fbe2320139de89

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
+# doc = Nokogiri::HTML(html)
+
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+new_doc = doc.css(".site-header__hero__headline")
+new_doc.text
+
+
+# refined_doc = doc.css(".site-header__hero__headline").text
+# refined_doc.text.strip
